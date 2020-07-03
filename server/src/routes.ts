393e1fef3f5bfc921d171062892b703e88ccd865
Lucas Rodrix <lucasrodrigues1985.lfr@gmail.com>
2020-07-03 11:36:55 -0300

    Realizado Cadastro do Ponto de Coleta

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -7,12 +7,31 @@ routes.get('/items', async (request, response) => {
     const items = await knex('items').select('*');
     const serializedItems = items.map(item => {
         return {
+            id: item.id,
             title: item.title,
             image_url: `http://localhost:3333/uploads/${item.image}`,
         }
     });
 
     return response.json(serializedItems);
+});
+
+routes.post('/points', async (request, response) => {
+    const {name, email, whatsapp, latitude, longitude, city, uf, items } = request.body;
+
+    const trx = await knex.transaction();
+    
+    const insertedIds = await knex('points').insert({image: 'image-fake', name, email, whatsapp, latitude, longitude, city, uf});
+
+    const point_id = insertedIds[0];
+
+    const poinItems = items.map((item_id: number) => {
+        return{item_id, point_id};
+    })
+
+    await knex('point_items').insert(poinItems);
+
+    return response.json({success: true});
 });
 
 export default routes;
